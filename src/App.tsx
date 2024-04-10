eb7b3856d08da21946671bed161fb9ed97018bf8
Yashkharel19 <yash048339@gmail.com>
2024-04-10 13:48:42 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// App.js
+
+import Header from './containers/header';
+// import Content from './Content';
+// import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/header.tsx
@@ -0,0 +1,11 @@
+// import Nav from "../components/navbar/nav";
+
+const Header = () => {
+  return (
+   <div>
+    {/* <Nav /> */}
+   </div>
+  );
+}
+
+export default Header;
