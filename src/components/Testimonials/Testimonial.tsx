591a2786bdef9f86468e81d6c9eecc9fb052c69e
Yashkharel19 <yash048339@gmail.com>
2024-04-25 17:31:32 -0400

    content added

--- src/components/Testimonials/Testimonial.tsx
+++ src/components/Testimonials/Testimonial.tsx
@@ -40,8 +40,18 @@ const Testimonial: React.FC = () => {
                       <img className="object-cover w-full h-full" src={testimonial.imageUrl} alt={testimonial.name} />
                     </div>
                     <div>
-                      <div className="font-bold text-xl mb-2">{testimonial.name}</div>
-                      <p className="text-gray-700 text-base">{testimonial.content}</p>
+                      <div className="text-gray-700 text-base mb-2 relative">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute top-0 left-0 transform -translate-y-1/2 -translate-x-1/2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2h1a1 1 0 110 2h-1v6a2 2 0 01-2 2H4a2 2 0 01-2-2V9H1a1 1 0 010-2h1V5zm2 0h12v2H4V5z" clipRule="evenodd" />
+                        </svg>
+                        <p className="inline">{testimonial.content}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute bottom-0 right-0 transform translate-x-1/2 translate-y-1/2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M18 5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V5zM4 3a1 1 0 00-1 1v8a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1H4z" clipRule="evenodd" />
+                        </svg>
+                      </div>
+                      <div className="text-gray-600 text-sm">
+                        - {testimonial.name}
+                      </div>
                     </div>
                   </div>
                 </div>
