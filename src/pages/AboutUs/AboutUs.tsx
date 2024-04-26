a042b594e96bca586d25554cb4e8fe395b57ace5
Yashkharel19 <yash048339@gmail.com>
2024-04-26 14:03:36 -0400

    content changed in the about us

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -16,7 +16,7 @@ const AboutUs: React.FC = () => {
       <div className="w-full md:w-1/2 px-4">
         <h2 className="text-2xl font-bold mb-4">About Us</h2>
         <p className="text-lg mb-4">
-          Your content goes here. Describe your company, mission, values, etc.
+        At KYRO, our journey is rooted in passion and purpose. As Ojaswi Rijal and Yash Kharel, two hearts united in a shared dream, we've embarked on a mission to provide dogs with the highest quality natural chews. Graduates from Nepal, with a history of entrepreneurship, we found inspiration in our love for animals, particularly dogs. Through diligent research and a stroke of serendipity, we discovered that our homeland was a treasure trove of natural chew manufacturers. Thus, KYRO was born - a fusion of our names, a symbol of unity, and a promise of excellence. With each chew, we strive not only to satisfy canine cravings but also to enrich the bond between humans and their four-legged companions. Welcome to KYRO, where every chew is crafted with love, care, and a touch of Nepali heritage.
         </p>
         {/* Add more content here */}
       </div>
