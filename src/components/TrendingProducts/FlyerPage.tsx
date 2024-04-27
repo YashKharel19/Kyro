783d34d35f373ba248a333a8e1e397d7ea40c91b
Yashkharel19 <yash048339@gmail.com>
2024-04-27 12:19:01 -0400

    content flyer added

--- src/components/TrendingProducts/FlyerPage.tsx
+++ src/components/TrendingProducts/FlyerPage.tsx
@@ -5,13 +5,15 @@ import './Flyer.css';
 const FlyerPage: React.FC = () => {
   return (
 
-
-    <div className="relative w-full h-40 md:h-60 lg:h-80 xl:h-96 overflow-hidden">
-      <img
-        src="../../../src/assets/preorder.svg"
-        alt="Flyer"
-        className="absolute top-0 left-0 w-full h-auto max-h-full object-contain animate-marquee"
-      />
+    <div className="mx-auto my-4">
+      <div className="relative w-full h-40 md:h-60 lg:h-80 xl:h-96 overflow-hidden">
+        <img
+          src="../../../src/assets/preorder.svg"
+          alt="Flyer"
+          className="absolute top-0 left-0 w-full h-auto max-h-full object-contain animate-zoom"
+          style={{ padding: '20px 0' }} // Add padding top and bottom
+        />
+      </div>
     </div>
   );
 };
