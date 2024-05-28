63fa3b2907b4b01e27e295052ae1d580b31efb74
Yashkharel19 <yash048339@gmail.com>
2024-05-28 17:44:12 -0400

    foxy.io integrated

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    readonly VITE_FOXY_STORE_DOMAIN: string;
+    readonly VITE_APP_FOXY_API_KEY: string;
+    readonly VITE_APP_TEST_VARIABLE: string;
+  
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
